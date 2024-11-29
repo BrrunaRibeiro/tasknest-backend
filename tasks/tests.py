c6bf5eccca8e22caa1ba61eaf35dac1f58e3433d
Bruna Ribeiro <brrunarib@gmail.com>
2024-11-29 15:15:19 +0100

    Add tests to the app

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from django.urls import reverse  
+from rest_framework.test import APITestCase  
+from rest_framework import status  
+from .models import Task  
 
-# Create your tests here.
+class TaskTests(APITestCase):  
+    """  
+    Contains tests for Task API endpoints.  
+    """  
+
+    def test_create_task(self):  
+        """  
+        Ensure we can create a new task.  
+        """  
+        url = reverse('task-list-create')  
+        data = {'title': 'Test Task', 'description': 'Description'}  
+        self.client.force_authenticate(user=None)  # Authenticate if necessary  
+        response = self.client.post(url, data, format='json')  
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)  
+        self.assertEqual(Task.objects.count(), 1)  
+        self.assertEqual(Task.objects.get().title, 'Test Task')  
