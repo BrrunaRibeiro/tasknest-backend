b95ae3dab4bf06a6c5e3b4f3c369634f75bb0a9b
Bruna Ribeiro <brrunarib@gmail.com>
2024-12-03 11:56:25 +0100

    Add Inline Categories in the Admin(Task) page

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin  
 from .models import Task, Category  
 
+# Inline model for managing categories directly from the Task admin  
+class CategoryInline(admin.TabularInline):  
+    model = Category  
+    extra = 1  # Number of empty forms to display for adding new categories  
+
 # Customize the Category admin  
 @admin.register(Category)  
 class CategoryAdmin(admin.ModelAdmin):  
@@ -10,7 +15,9 @@ class CategoryAdmin(admin.ModelAdmin):
 # Customize the Task admin  
 @admin.register(Task)  
 class TaskAdmin(admin.ModelAdmin):  
-    list_display = ('id', 'title', 'priority', 'state', 'due_date', 'is_overdue', 'category')  # Fields to display in the task list  
-    list_filter = ('priority', 'state', 'category', 'is_overdue')  # Add filters for priority, state, and category  
+    list_display = ('id', 'title', 'priority',
+                     'state', 'due_date', 'is_overdue', 'category')  # Fields to display in the task list  
+    list_filter = ('priority', 'state', 'category', 'is_overdue')  # Add filters for priority, state, category, and overdue status  
     search_fields = ('title', 'description')  # Add a search bar for tasks  
     filter_horizontal = ('owners',)  # Add a horizontal filter for the ManyToManyField (owners)  
+    inlines = [CategoryInline]  # Include the inline category management  
