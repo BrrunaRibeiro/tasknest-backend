4455b4fa74efcaafdb8bed765c7e0382292b93ad
Bruna Ribeiro <brrunarib@gmail.com>
2024-12-04 14:17:55 +0100

    Add url path for LoginView

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path  
 from .views import (  
     TaskListCreateView, TaskRetrieveUpdateDestroyView,  
-    CategoryListCreateView, RegisterView, 
+    CategoryListCreateView, RegisterView, LoginView
 )  
 
 urlpatterns = [  
