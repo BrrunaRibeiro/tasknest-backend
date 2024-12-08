07d434c8dacddd98a55d5fb419eb8a96ba1fc415
Bruna Ribeiro <brrunarib@gmail.com>
2024-12-08 11:39:46 +0100

    Add CreateTask path to urls.py

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path  
 from .views import (  
     TaskListCreateView, TaskRetrieveUpdateDestroyView,  
-    CategoryListCreateView, RegisterView, LoginView, LogoutView, check_email, CheckAuthView
+    CategoryListCreateView, RegisterView, LoginView, LogoutView, check_email,
+    CheckAuthView, TaskCreateView, TaskListView
 )  
 
 urlpatterns = [  
-    path('tasks/', TaskListCreateView.as_view(), name='task-list'),  
+    path('tasks/', TaskListView.as_view(), name='task-list'),  
     path('tasks/<int:pk>/', TaskRetrieveUpdateDestroyView.as_view(), name='task-detail'),
+    path('tasks-create/', TaskCreateView.as_view(), name='task-create')
     path('categories/', CategoryListCreateView.as_view(), name='category-list-create'),
     path('register/', RegisterView.as_view(), name='register'),
     path('check-email/', check_email, name='check-email'),
